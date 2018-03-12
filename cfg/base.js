65d240da73ce6e4c7bb36916d094b9ed47748db9
李国梁(北京研发中心) <guoliangli213070@sohu-inc.com>
2018-03-12 23:58:21 +0800

    修改滤镜

--- cfg/base.js
+++ cfg/base.js
@@ -17,7 +17,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/../dist/assets'),
     filename: 'app.js',
-    publicPath: './assets/'
+    publicPath: 'assets/'
   },
   devServer: {
     contentBase: './src/',
